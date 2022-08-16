707c9cee340bde1ddd2b5d5ae38d14af8ff639ba
David Centurion <dencen9@gmail.com>
2022-08-16 15:55:24 -0400

    before heroku

--- src/modules/displayData.js
+++ src/modules/displayData.js
@@ -1,14 +1,6 @@
 import getData from "./getData";
 
 export default class weather{
-  constructor (city, country, temp, sky, tempFeel, wind){
-    this.city = city;
-    this.country = country;
-    this.temp = temp;
-    this.sky = sky;
-    this.tempFeel = tempFeel;
-    this.wind = wind;
-  }
   static searchLocation(){
     const container = document.createElement('div');
     container.classList.add('search-container');
@@ -24,26 +16,34 @@ export default class weather{
     return container;
   }
   static containerFill (weather){
-    const temp = (weather.temperature - 273.15).toFixed(1);
-    const tempFeel = (weather.feelsLike - 273.15).toFixed(1);
-    let weatherIconClass = '';
-    // TO change weather icons according to conditions
-    if(weather.sky === 'Clouds')weatherIconClass = 'clouds'
-    else if(weather.sky === 'Clear')weatherIconClass = 'clear'
-    else if(weather.sky === 'Rain' || weather.sky=='Drizzle')weatherIconClass = 'rain'
-    else if(weather.sky === 'Thunderstorm')weatherIconClass = 'thunderstorm'
+    let innerHTML;
+    console.log('code succes', weather.cod)
+    if(weather.cod !== 200){
+      return 'Somthing went wrong or city not found';
+    }
 
-    const innerHTML = `
-    <h2 class="city">${weather.cityName}, ${weather.country}</h2>
-    <h2 class="temperature">${temp} - <span class="sky">${weather.sky}</span></h2>
-    <i class="weather-icon ${weatherIconClass}"></i>
-    <p class="sky-desc">${weather.skyDesc}</p>
-    <p class="feeling">feels like ${tempFeel}</p>
-    <p class="humidity">humidity ${weather.humidity}%</p>
-    <p class="wind-speed">wind ${weather.windSpeed} m/s</p>
-  `
-  return innerHTML;
-  }
+      const temp = (weather.temperature - 273.15).toFixed(1);
+      const tempFeel = (weather.feelsLike - 273.15).toFixed(1);
+      let weatherIconClass = '';
+      // TO change weather icons according to conditions
+      if(weather.sky === 'Clouds')weatherIconClass = 'clouds'
+      else if(weather.sky === 'Clear')weatherIconClass = 'clear'
+      else if(weather.sky === 'Rain' || weather.sky=='Drizzle')weatherIconClass = 'rain'
+      else if(weather.sky === 'Thunderstorm')weatherIconClass = 'thunderstorm'
+      
+      innerHTML = `
+      <h2 class="city">${weather.cityName}, ${weather.country}</h2>
+      <h2 class="temperature">${temp}°C - <span class="sky">${weather.sky}</span></h2>
+      <i class="weather-icon ${weatherIconClass}"></i>
+      <p class="sky-desc">${weather.skyDesc}</p>
+      <p class="feeling">feels like ${tempFeel}°C</p>
+      <p class="humidity">humidity ${weather.humidity}%</p>
+      <p class="wind-speed">wind ${weather.windSpeed} m/s</p>
+      `
+    return innerHTML;
+
+}
+  
   static dataContainer(weather){
     const container = document.createElement('div');
     container.classList.add('weather-container');
--- src/modules/getData.js
+++ src/modules/getData.js
@@ -3,21 +3,27 @@ export default class data {
     const API_KEY = '18123aaa20b41be0c901f11c5f07d7d8'
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`)
     const data = await response.json();
+    if(data.cod !== 200){
+      console.log('error', data);
+      return data;
+    }
     console.log(data);
     const weather = {
-      'cityName':data.name,
-      'country':data.sys.country,
-      'temperature':data.main.temp,
-      'feelsLike':data.main.feels_like,
-      'sky':data.weather[0].main,
-      'skyDesc':data.weather[0].description,
-      'humidity':data.main.humidity,
-      'windSpeed':data.wind.speed,
-      'time':data.dt,
-      'timeZone':data.timezone,
+    'cityName':data.name,
+    'country':data.sys.country,
+    'temperature':data.main.temp,
+    'feelsLike':data.main.feels_like,
+    'sky':data.weather[0].main,
+    'skyDesc':data.weather[0].description,
+    'humidity':data.main.humidity,
+    'windSpeed':data.wind.speed,
+    'time':data.dt,
+    'timeZone':data.timezone,
+    'cod':data.cod
+    
+  }
+  console.log('weather', weather);
+  return weather;
 
-    }
-    console.log('weather', weather);
-    return weather;
   }
 } 
